5c51c8c3de991e0ddd17dc959e304eebdb2f2ce8
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2021-04-24 04:09:06 +0530

    :art: Get initial name when opening the page

--- static/js/submit.js
+++ static/js/submit.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "/get_birmd",
+        dataType: "json",
+    }).done(function (data) {
+        $("#birmd-name").text(data["birmd_name"]);
+    });
+
     $("form").on("submit", function (event) {
         $.ajax({
             type: "GET",
