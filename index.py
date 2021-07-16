9573edf19b850d50bacab0920e32651da8329e2a
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2021-07-17 00:26:58 +0530

    Add footer

---
+++ index.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, jsonify
+from textgenrnn import textgenrnn
+import os
+
+
+app = Flask(__name__)
+
+
+TEXTGENRNN = textgenrnn(
+    weights_path="weights/birmd_weights.hdf5",
+    vocab_path="weights/birmd_vocab.json",
+    config_path="weights/birmd_config.json",
+)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get_birmd", methods=["GET"])
+def get_birmd():
+    birmd_name = TEXTGENRNN.generate(return_as_list=True, temperature=1.2)[0]
+    return jsonify({"birmd_name": birmd_name})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
